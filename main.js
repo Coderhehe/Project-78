4dd8b0a697c0eb7f7dbb36f69c60c6ab1d88d35b
Coderhehe <77240747+Coderhehe@users.noreply.github.com>
2021-03-14 14:52:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
+var names= [
+    "Ashu","Aryan"
+];
 var images = [
     "ashu's photo.jpeg","3ba6bbc6-30aa-4a66-ba3b-64de75959882.jpg"
     ]; 
     var i=0;
-    function nextone(){ document.getElementById("img").src=images[i];
+    function nextone(){ document.getElementById("img").src=images[i]; document.getElementById("h4").innerHTML=names[i];
     i++;
     if(i==2){i=0;}}
